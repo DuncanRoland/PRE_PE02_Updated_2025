aea6766971b3438bfafc84f080435886a91ce89d
Duncan Roland <duncan.roland2000@gmail.com>
2025-12-03 08:56:28 +0100

    Make the Person class

---
+++ Pre.PEJobApplicationSystem/Pre.PEJobApplicationSystem.Cons/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Pre.PEJobApplicationSystem.Cons;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Pre.PEJobApplicationSystem/Pre.PEJobApplicationSystem.Core/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Pre.PEJobApplicationSystem.Core;
+
+public abstract class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; private set; }
+    public string Email { get; set; }
+
+    protected Person(string firstName, string lastName, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+    
+    public string GetFullName() => $"Full name: {FirstName} {LastName}";
+
+    public string GetInfo()
+    {
+        return GetFullName();
+    }
+}
