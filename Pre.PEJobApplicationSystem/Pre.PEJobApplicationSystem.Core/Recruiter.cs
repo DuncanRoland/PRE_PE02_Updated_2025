775905b39ec59322d435d1118867baa6b0d25e9d
Duncan Roland <duncan.roland2000@gmail.com>
2025-12-03 09:00:53 +0100

    Make the Recruiter class

---
+++ Pre.PEJobApplicationSystem/Pre.PEJobApplicationSystem.Core/Recruiter.cs
@@ -0,0 +1,18 @@
+namespace Pre.PEJobApplicationSystem.Core;
+
+public class Recruiter
+{
+    public Recruiter(string firstName, string lastName, string email)
+    {
+    }
+
+    public void PostJob(Company company, Job job)
+    {
+        
+    }
+
+    public void ReviewApplication(JobApplication application)
+    {
+        
+    }
+}
